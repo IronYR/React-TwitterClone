d55e55d83dec0470b6ce8c9988f58d0fb3725c58
IronYR <yrehan67@gmail.com>
2020-08-25 17:45:09 +0300

    testa

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -4,7 +4,7 @@ import classes from './Header.module.css';
 export default function Header(props) {
     return (
         <div className={classes.header}>
-            {props.showBackButton ? <img onClick={props.goBack} src={Back} alt=""/>: null}
+            {props.showBackButton ? <img onClick={props.goBack} src='' alt=""/>: null}
             <span className={classes.title}>{props.title}</span>
             <button onClick={props.logout} className={classes.logout}>Log Out</button>
         </div>
